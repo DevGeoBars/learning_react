87b210e3b08c669122fce546d2baddc75746197d
geo bars <georgiybarsegian@yandex.com>
2026-01-20 22:07:27 +0300

    hooks / useLayoutEffects: -что когда используется

--- src/hooks/ReactBuiltInHooks/effectHooks/useLayoutEffect/index.tsx
+++ src/hooks/ReactBuiltInHooks/effectHooks/useLayoutEffect/index.tsx
@@ -1,9 +1,25 @@
-import { FC } from "react";
+import {FC, useEffect, useInsertionEffect, useLayoutEffect} from "react";
 
 type TUseLayoutEffectExampleProps = {
 
 }
 export const UseLayoutEffectExample: FC<TUseLayoutEffectExampleProps>  = ({}) => {
+
+  // 1. Начинайте всегда с useEffect (для всего)
+  useEffect(() => { /* загрузка данных, подписки */
+  });
+
+  // 2. Переходите на useLayoutEffect ТОЛЬКО если:
+  //    - Нужно измерить DOM перед отрисовкой
+  //    - Видите "мигание" при обновлении состояния
+  useLayoutEffect(() => { /* измерение элементов */
+  });
+
+  // 3. useInsertionEffect используйте ТОЛЬКО если пишете библиотеку CSS-in-JS
+  useInsertionEffect(() => { /* вставка стилей */
+  });
+
+
   return <>
 
   </>
