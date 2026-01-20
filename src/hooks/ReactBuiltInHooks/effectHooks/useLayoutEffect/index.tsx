f963f0495d822672e4ab416dc4de7bd5bfb1c8ad
geo bars <georgiybarsegian@yandex.com>
2026-01-20 22:02:57 +0300

    hooks / useLayoutEffects: -простой пример

---
+++ src/hooks/ReactBuiltInHooks/effectHooks/useLayoutEffect/index.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+type TUseLayoutEffectExampleProps = {
+
+}
+export const UseLayoutEffectExample: FC<TUseLayoutEffectExampleProps>  = ({}) => {
+  return <>
+
+  </>
+};
+
